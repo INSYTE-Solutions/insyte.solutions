67dffb63e329de2a5275399f00ea652f9b5f4beb
marksyte <mark@insyte.solutions>
2025-05-22 21:57:56 +0200

    Contact button colours
    Signed-off-by: marksyte <mark@insyte.solutions>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -64,7 +64,7 @@ const NavBar = forwardRef<HTMLDivElement>(
                 <Link
                   key="Contact"
                   href="#contact" 
-                  className="bg-primary text-primary-foreground hover:bg-primary/90 hidden md:inline-flex bg-gradient-to-r from-teal-500 to-blue-600 hover:from-teal-600 hover:to-blue-700 h-10 px-4 py-2 rounded-md">
+                  className="bg-primary text-primary-foreground hover:bg-primary/90 hidden md:inline-flex bg-gradient-to-r from-[#0e7270] to-[#468872] hover:from-[#258849] hover:to-[#60be7a] h-10 px-4 py-2 rounded-md">
                   Contact
                 </Link>
 
